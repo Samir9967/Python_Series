06bee65d994f7358208c185a4159f05130b2734a
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-08-08 16:50:49 +0000

    commit

---
+++ 02_keywords/keywords.py
@@ -0,0 +1,8 @@
+# (import keyword)  is used to get all keywords in python
+
+import keyword
+
+keywords = keyword.kwlist   
+#Certainly! In Python, the keyword module provides the kwlist attribute, which is a list containing all the keywords used in the language. You can use this attribute to access the list of Python keywords.
+print(keywords)
+print(len(keywords))   

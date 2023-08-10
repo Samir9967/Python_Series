79a764e6d9a689d2e3604aa998b0d0fd7055c36d
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-08-10 20:36:55 +0000

    commit

---
+++ 03_data_types/int.py
@@ -0,0 +1,28 @@
+# Integer (Numeric)
+
+a = 12
+# print(type (a))
+
+b = -234
+# print(type (b))
+
+# c = 12.34              # this will give type float
+# print(type (c))       
+
+
+# convert decimal to binary 
+number = 12
+num = bin(number)
+print(type(num))
+# print(bin(number))   #bin() is used to convert in binary number
+
+# convert binary to octal
+binary = 0b1111111
+binary2 = oct(binary)
+print(type(binary2))
+# print(oct(binary))
+
+
+# convert decimal to hexadecimal
+decimal = 13
+print(hex(decimal))

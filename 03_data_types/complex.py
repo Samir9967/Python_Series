bea59f9b44a424003fe8c5bd04614fcbdc9d8508
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-08-11 09:33:54 +0000

    complex commit

---
+++ 03_data_types/complex.py
@@ -0,0 +1,24 @@
+# a + bj--------------------> here a is real and b is imaginary
+
+# num = 12 + 13j
+# print(type(num))
+# print(num)
+
+# num = 12.34 + 13j            # with float
+# print(num)
+
+# num = 0b11111 + 13j          # with binary
+# print(num)
+
+# num = 0o1234 + 13J           # with octal
+# print(num)
+
+num = 0x1235aab + 13j         # with hexadecimal
+print(num)
+
+# num = 12 + 0b111j              # imaginary with binary its will be syntax error
+
+num1 = 12 + 13j
+num2 = 12 + 13j
+num3 = num1 + num2
+print(num3) 

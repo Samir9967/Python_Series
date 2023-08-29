a4f1c1ee1ca267baf7dcbfe21e15ee7771b07d68
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-08-29 16:35:42 +0000

    commit

---
+++ 08_Operators/1_|Arithmethic.py
@@ -0,0 +1,32 @@
+# Arithmetic operators
+
+num1 = 50
+num2 = 12
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 % num2)
+
+# print(num1 // num2)
+
+# a = 50.0
+# b = 2
+# print(a / b)
+# print(a // b)
+
+                                #other example
+# name = "Samir"
+# num = 10
+# print(name + num)           # error
+
+# a = 10
+# b = 0
+# print(a / b)                   #error
+
+a = "Samir"
+print(a + "23")
+
+# print(a - "23")          #error
+
+print(a * 3)

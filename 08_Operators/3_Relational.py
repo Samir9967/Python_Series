7100ec30894424adb34e8cd1fc42010ff370dfdf
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-08-30 07:56:12 +0000

    commit

---
+++ 08_Operators/3_Relational.py
@@ -0,0 +1,15 @@
+# relational operator with string
+
+# name = "Samir"
+# name1 = "Maharana"
+# print(name>name1)
+
+name = "1233"
+name2 = "1237"
+print(name > name2)
+
+
+# between int and string
+# print(200 > "Samir")                # error
+
+print("Samir" == "SAMIR")

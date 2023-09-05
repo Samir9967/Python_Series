dc0f7820354ed19d00561ebf656b55417d626df3
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-05 16:41:53 +0000

    commit

---
+++ 08_Operators/4_Logical.py
@@ -0,0 +1,25 @@
+# Logical operator -----------------> and, or, not
+        # with boolean value
+                                 #and
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+                                  # or
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+                                 # not
+print(not True)
+print(not False)
+
+                                   # with non boolean value
+print(12 and 10)
+print("Samir" and 12)
+print(0 and 12)
+
+
+print("Samir" or "Maharana")
---
+++ 08_Operators/5_unary_minus.py
@@ -0,0 +1,8 @@
+#(-)
+
+num = 14
+print(-num)
+
+num1 = -12
+print(type (num1))
+print(-num1)

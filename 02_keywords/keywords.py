18e2a9e5d82ff64b0145713e820923a7da21538d
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-06 05:26:18 +0000

    commit

--- 02_keywords/keywords.py
+++ 02_keywords/keywords.py
@@ -2,7 +2,7 @@
 
 import keyword
 
-keywords = keyword.kwlist   
+keywordss = keyword.kwlist   
 #Certainly! In Python, the keyword module provides the kwlist attribute, which is a list containing all the keywords used in the language. You can use this attribute to access the list of Python keywords.
-print(keywords)
-print(len(keywords))   
+print(keywordss)
+print(len(keywordss))   
---
+++ 08_Operators/6_IdentityOperator.py
@@ -0,0 +1,21 @@
+# is, is not
+
+num = 12
+num1 = 12
+print(num is num1)
+print(id(num), id(num1))
+
+# num2 = 123
+# num3 = 124
+# print(num2 is num3)
+# print(id(num2), id(num3))
+
+# num2 = 123
+# num3 = 124
+# print(num2 is not num3)
+# print(id(num2), id(num3))
+
+num2 = 123
+num3 = 123
+print(num2 is not num3)
+print(id(num2), id(num3))

ea0484ca653d1a7774e39836162ad734bcca9284
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-06 07:15:39 +0000

    commit

---
+++ 10_Conditional_Statement/If_Else.py
@@ -0,0 +1,6 @@
+marks = int(input("Enter the marks: "))
+
+if(marks >= 35):
+    print("Congrat.............pass")
+else:
+    print("Opps..............fail")
---
+++ 10_Conditional_Statement/if_elif_else.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter the marks between 1 to 5: "))
+
+if(marks>=0):
+ if(marks == 1):
+    print("A grade")
+ elif(marks == 2):
+    print("B grade")
+ elif(marks == 3):
+    print("C grade")
+ elif(marks == 4):
+    print("D grade")
+ elif(marks == 5):
+    print("E grade")
+else:
+    print("Invalid enter number 1 to 5")

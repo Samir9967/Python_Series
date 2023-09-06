6f684c6cab3bcf759318481e07a6ade1ce9a5e6e
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-06 06:19:39 +0000

    commit

---
+++ 08_Operators/7_Membership_operator.py
@@ -0,0 +1,8 @@
+# in, not in
+name = "Samir Maharana"
+print("Sa" in name)
+
+print("samir" not in name)
+
+names = ["sam","maharana",12,234,567,11]
+print(12 in names)
---
+++ 09_DynamicInput_UserInput/UserInput.py
@@ -0,0 +1,14 @@
+# user input
+
+num1 = input("Enter number 1: ")                           # it will give string type
+num2 = input("Enter number 2: ")
+total= num1 + num2
+print(total)
+print(type(total))
+                        
+                          # convert to int by typecasting (implicit)
+# num1 = int(input("Enter number 1: "))                           # it will give string type
+# num2 = int(input("Enter number 2: "))
+# total= num1 + num2
+# print(total)
+# print(type(total))

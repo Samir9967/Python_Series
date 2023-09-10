4e279451cb74709b0bbe4ac0d4d91f0a94b1fc47
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-10 07:40:55 +0000

    commit

--- 10_Conditional_Statement/If_Else.py
+++ 10_Conditional_Statement/If_Else.py
@@ -1,6 +1,11 @@
-marks = int(input("Enter the marks: "))
+# marks = int(input("Enter the marks: "))
 
-if(marks >= 35):
-    print("Congrat.............pass")
-else:
-    print("Opps..............fail")
+# if(marks >= 35):
+#     print("Congrat.............pass")
+# else:
+#     print("Opps..............fail")
+
+                                # id()
+name = "Samir"
+name2 = "Samir"
+print(id(name), id(name2))

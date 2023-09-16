a50533125f6252176cde441a77cff851a0e1b9e6
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-16 08:08:12 +0000

    commit

---
+++ 12_Looping/While.py
@@ -0,0 +1,6 @@
+# print 10 to 1
+
+num = 10
+while(num > 0):
+    print(num)
+    num = num -1
---
+++ 12_Looping/While2.py
@@ -0,0 +1,7 @@
+# print the number series like 111 222 333 444 555 666
+
+number = 111
+
+while(number <= 666):
+    print(number)
+    number = number + 111  

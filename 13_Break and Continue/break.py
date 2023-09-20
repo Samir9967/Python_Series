fd6fd68e55f9143b6163e386a7883bcad772ded9
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-20 19:40:31 +0000

    commit

---
+++ 13_Break and Continue/break.py
@@ -0,0 +1,4 @@
+for num in range(1,11,1):
+    if(num == 3):
+        break
+    print(num)
---
+++ 13_Break and Continue/continue.py
@@ -0,0 +1,4 @@
+for num in range(1,11,1):
+    if(num == 3 or num == 6):
+        continue
+    print(num)

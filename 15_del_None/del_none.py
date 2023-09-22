1322ed8ef1e08f45ab0fa9f6fb2518b781f006e6
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-22 04:58:55 +0000

    commit

---
+++ 15_del_None/del_none.py
@@ -0,0 +1,14 @@
+# name = "Samir"
+# print(name)
+# del name
+# print(name)
+
+name = ["samir","maharana","mom","dad"]
+print(name)
+del name[0]
+print(name)
+                      # None
+num = 123
+print(num)
+num = None
+print(num)

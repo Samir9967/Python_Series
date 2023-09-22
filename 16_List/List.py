0bf555156cd68862ac058314144a79004e727873
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-22 21:23:07 +0000

    commit

---
+++ 16_List/List.py
@@ -0,0 +1,20 @@
+                   # Creating empty list
+# num = []
+# print(num)
+# print(type(num))
+
+                  # dynamically list using eval()
+# num = eval(input("Enter the list:  "))
+# print(num)
+# print(type(num))
+
+                    # creating list using range()
+# num = list(range(0,5,1))
+# print(num)
+# print(type(num))
+
+                         # creating listusing split()
+message ="Welcome Samir Maharana"
+msg = message.split()
+print(msg)
+print(type(msg))

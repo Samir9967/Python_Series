e0624b3c5f98b8793e5e3561026a2fff66e7cc2b
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-24 19:58:23 +0000

    commit

--- 17_List_Method/listMethod.py
+++ 17_List_Method/listMethod.py
@@ -2,5 +2,13 @@ num = [12,34,56,78,12]
 print(num)
 
                   # append()
-num.append(13)
-print(num)
+# num.append(13)
+# print(num)
+
+                # insert()
+# num.insert(1,"samir")
+# print(num)
+
+                    # count()
+total_count = num.count(12)
+print(total_count)
---
+++ 17_List_Method/listPractise.py
@@ -0,0 +1,15 @@
+# write a program to create a list by adding 20  0's
+
+# num = []
+
+# for i in range(0,20,1):
+#     num.append(0)
+
+# print(num)
+
+# write a program to create a list of 20 adding 0 to 20 number
+
+num2 = []
+for i in range(0,21,1):
+    num2.append(i)
+print(num2)

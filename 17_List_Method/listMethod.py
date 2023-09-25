42766e7e77570b00d8d6ca8267147525577ec7d1
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-09-25 19:22:08 +0000

    commit

--- 17_List_Method/listMethod.py
+++ 17_List_Method/listMethod.py
@@ -18,5 +18,20 @@ print(num)
 # print(index_num)
 
                           # remove()
-num.remove(12)
+# num.remove(12)
+# print(num)
+
+                         # pop()
+# num.pop()
+# print(num)
+# num.pop(1)
+# print(num)
+                          # extends()
+# list1 = [1,2,3,4,5]
+# list2 = [6,7,8,9]
+# list1.extend(list2)
+# print(list1)
+
+                          # reverse()
+num.reverse()
 print(num)

8a856b5c354b26a41bb3ea313b069c95ab9c195b
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-07 09:30:12 +0000

    commited

---
+++ 20_List_Comparision/listComparision.py
@@ -0,0 +1,13 @@
+# using equality operator
+# name_1 = ['samir','maharana','panda']
+# name_2 = ['Samir','Maharana','panda']
+# name_3 = ['samir','maharana','panda']
+
+# print(name_1 == name_2)
+# print(name_1 == name_3)
+
+# using relational operator
+num = [10,11,12]
+num_2 = [11]
+
+print(num > num_2)

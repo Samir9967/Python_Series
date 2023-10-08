0bfe1d98f2d156ad154abb26c8dc2858f49b3458
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-08 08:53:37 +0000

    commit

--- 21_Nested_List/nested_list.py
+++ 21_Nested_List/nested_list.py
@@ -1,4 +1,31 @@
-num = [1,2,3,4,[5,6,7],8,9]
-print(num)
-print(num[4])
-print(num[4][1])
+# num = [1,2,3,4,[5,6,7],8,9]
+# print(num)
+# print(num[4])
+# print(num[4][1])
+
+# num2 = [1,2,3,[4,5,6,[7,8,9]],10,11,12]
+# print(num2[3])
+# print(num2[3][1])
+# print(num2[3][3])
+# print(num2[3][3][2])
+
+                           # nested as a matrix
+num3 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+# print(num3[1])
+# print(num3)
+
+# for i in num3:
+#     print(i)
+
+# for i in num3:
+#     for j in i:
+#         print(j,end=' ')
+#     print(" ")
+
+num2 = [1,2,3,[4,5,6,[7,8,9]],10,11,12]
+num2.reverse()
+print(num2)
---
+++ 22_List_comprehension/listComprehension.py
@@ -0,0 +1,15 @@
+# num = [i*i for i in range(0,11)]
+# print(num)
+
+names = ['samir','maharana','aditi','ghadi','sum']
+# expected output = ]['s','m','a','g']
+
+# l = [i for i in names if 'a' in i]
+# print(l)
+
+# l = [i  if i !='samir' else 'sam' for i in  names]
+# print(l)
+
+                                   # creation of matrix using list comprehension
+l = [[j for j in range(3)] for i in range(3)]
+print(l)

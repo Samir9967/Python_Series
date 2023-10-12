3b7a5515e66f38bf39e385d4ae8f74ea917cb8b9
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-12 13:33:40 +0000

    commit

---
+++ 24_Tupple/tupple.py
@@ -0,0 +1,17 @@
+                                     # creation of empty tuple
+tup = ()
+# print(type(tup), tup)
+
+                                    # creation of single value 
+tup2 = (23)               # without comma
+# print(type(tup2),tup2)          # it will give int without comma
+tup3 = (23,)               # with comma
+# print(type(tup3),tup3)
+
+                     # creation of tuple with different datatypes of values
+tup4 = (1,2,"samir",True,12.45,[2,3,4])
+# print(tup4)
+
+                     # creation of tuple without ()
+tup5 = 10,20,30,[10,20],"samir",False,12.34
+print(type(tup5),tup5)

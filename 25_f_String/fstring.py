47f765c0a4cabe4eac1258ceb394fa361e7a3269
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-14 10:52:38 +0000

    commit

---
+++ 25_f_String/fstring.py
@@ -0,0 +1,3 @@
+name = "samir maharana"
+age = 24
+print(f"My name is {name} and my age is {age}")
---
+++ 26_Set/set.py
@@ -0,0 +1,15 @@
+# l = {}
+# print(l)
+# print(type(l))                # it will give dict
+ 
+                            # using set() function to create empty set
+# l = set()
+# print(type(l),l)
+
+# l = {12,23,4,5,67,8}
+# print(type(l),l)
+
+                   # creation of set using set() from list
+l = [12,23,45,67,89,True]
+s = set(l)
+print(type(s),s)

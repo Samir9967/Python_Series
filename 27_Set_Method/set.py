39bc183e8fc3c87bc93fe25759c778802a029692
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-15 08:52:31 +0000

    commit

---
+++ 27_Set_Method/set.py
@@ -0,0 +1,33 @@
+                               # add()
+# s = {12,34,56,78,90}
+# s.add("samir")
+# print(s)
+
+                                # update()
+# s = {1,2,3,4,5}
+# t = ['sam',12,34,1,2]
+# l = (12,23,45,True,False)
+# s.update(t,l,range(1,21))
+# print(s)
+# print(len(s))
+
+                               #remove()
+# s = {1,2,3,4,5,6}
+# s.remove(5)
+# print(s)
+
+                                  # discard()
+# s = {1,2,3,4,5,6}
+# s.discard(78)
+# print(s)
+
+                                    # pop()
+# s = {1,2,3,4,5,6}
+# s.pop()
+# print(s)
+
+                                     # copy()
+s = {1,2,3,4}
+s1 = s.copy()
+print(s1)
+print(id(s),id(s1))

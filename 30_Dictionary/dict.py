dceb518e20cc8304ad6c6210f24fcc1dad6a9e33
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-26 16:25:40 +0000

    commit

---
+++ 30_Dictionary/dict.py
@@ -0,0 +1,10 @@
+# myinfo = {'name':'samir','age':24,'gf':'adu'}
+# print(myinfo,type(myinfo))
+# print(myinfo['gf']) 
+
+
+                  # create empty dict and then add item
+myinfo = {}           # --> this is empty dict
+myinfo['name'] = 'adu'
+myinfo['age'] = 20
+print(myinfo)

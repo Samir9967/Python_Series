5f5967862d529977eb4c4b872508ddee28752edb
Samir Maharana <127733415+Samir9967@users.noreply.github.com>
2023-10-28 20:25:45 +0000

    commit

---
+++ 30_Dictionary/dict-Method.py
@@ -0,0 +1,33 @@
+                             # get()
+# d = {1:'sam',2:"adu",3:'True',3:False}
+# val = d.get(1)
+# print(val)
+# print(d.get(5))
+# print(d.get(5,'love u adu'))
+# print(d)
+
+                              # items()
+# print(d.items())
+# v = d.items()
+# print(v,type(v))
+# for i in d.items():
+#     print(i)
+# for i, j in d.items():
+#     print(i,j)
+
+                                    # pop()
+# d = {1:'sam',2:"adu",3:'True',3:False}
+# print(d)
+# d.pop(3)
+# print(d)
+
+                                      # popitem()
+# print(d)
+# d.popitem()
+# print(d)
+
+                                     # keys()
+d = {1:'sam',2:"adu",3:'True',3:False}
+# print(d.keys())
+for i in d.keys():
+    print(i)
